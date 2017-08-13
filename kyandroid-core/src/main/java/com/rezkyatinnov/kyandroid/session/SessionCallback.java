bd6610cee875af9b454921e5a273ff533f23abb6
Mohamad Rezkya Feurstinnov Hairy <rezkyatinnov@gmail.com>
2017-08-13 21:33:09 +0800

    SessionCallback has own methods

--- kyandroid-core/src/main/java/com/rezkyatinnov/kyandroid/session/SessionCallback.java
+++ kyandroid-core/src/main/java/com/rezkyatinnov/kyandroid/session/SessionCallback.java
@@ -1,10 +1,11 @@
 package com.rezkyatinnov.kyandroid.session;
 
-import io.realm.Realm;
-
 /**
  * Created by rezkya on 8/13/17.
  */
 
-public abstract class SessionCallback implements Realm.Transaction.OnSuccess, Realm.Transaction.OnError {
+public interface SessionCallback {
+    void onError(Throwable error);
+
+    void onSuccess();
 }
